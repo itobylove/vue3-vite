17d3ccfbcd6f2b06cbf90e87b1c5174a7656b094
黄立灼 <1637556100@qq.com>
2022-09-20 18:26:42 +0800

    图表

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,39 +1,45 @@
 <template>
     <div>
-        <el-row  class="min-h-screen bg-purple-400">
-            <el-col :md="12" :lg="16" class="flex items-center justify-center">
+        <el-row  class="container">
+            <el-col :lg="16" :md="12"  class="flex items-center justify-center">
                 <div class="text-light-50">
                     <div class=" font-bold text-5xl mb-4">欢迎光临</div>
                     <div>此站是《vue3 + vite实战商城后台开发》演示项目demo</div>
                 </div>
             </el-col>
-            <el-col :span="12" :lg="8" class="bg-lime-200 flex justify-center items-center">
-              <div>
-                <h2>欢迎回来</h2>
-                <div>
-                  <span></span>
-                  <span>账号密码登录</span>
-                  <span></span>
-                </div>
-                <div>
-                  <el-form
-                  ref="ruleFormRef"
-                  :model="ruleForm"
-                  :rules="rules"
-                  label-width="120px"
-                  class="demo-ruleForm"
-                  :size="formSize"
-                  status-icon
-                  >
-                    <el-form-item label="Activity name" prop="username">
-                      <el-input v-model="ruleForm.username" />
-                    </el-form-item>
-                    <el-form-item>
-                      <el-button type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
-                    </el-form-item>
-                  </el-form>
-                </div>
+            <el-col :lg="8" :md="12"  class=" bg-light-50 flex items-center justify-center flex-col">
+              <h2 class=" font-bold text-3xl text-gray-800">欢迎回来</h2>
+              <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
+                <span class="h-[1px] w-16 bg-gray-200"></span>
+                <span>账号密码登录</span>
+                <span class="h-[1px] w-16 bg-gray-200"></span>
               </div>
+              <el-form
+                ref="ruleFormRef"
+                :model="ruleForm"
+                :rules="rules"
+                class="demo-ruleForm w-[250px]"
+                :size="formSize"
+                status-icon
+                >
+                  <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" placeholder="请输入账号">
+                    <template #prefix>
+                      <el-icon class="el-input__icon"><User /></el-icon>
+                    </template>
+                    </el-input>
+                  </el-form-item>
+                  <el-form-item  prop="password">
+                    <el-input v-model="ruleForm.password" placeholder="请输入密码" >
+                    <template #prefix>
+                      <el-icon class="el-input__icon"><Lock /></el-icon>
+                    </template>
+                    </el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button class="w-[250px]" color="#626aef" round type="primary" @click="submitForm(ruleFormRef)">登录</el-button>
+                  </el-form-item>
+                </el-form>
             </el-col>
         </el-row>
     </div>
@@ -41,22 +47,34 @@
 
 <script setup>
   import { reactive, ref } from 'vue'
+  import { Lock, User } from '@element-plus/icons-vue'
   const ruleForm = reactive({
     username:'',
     password:''
   })
   const formSize = ref();
+  const ruleFormRef = ref();
   const rules = reactive({
     username:[
-      { required: true, message: 'Please input Activity name', trigger: 'blur' },
+      { required: true, message: '账号不能为空', trigger: 'blur' },
       { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+    ],
+    password:[
+      { required: true, message: '密码不能为空', trigger: 'blur' },
+      { min: 3, max: 5, message: '密码长度需3~5位', trigger: 'blur' },
     ]
   })
-  const submitForm = ()=>{
-    console.log(123);
+  const submitForm = (formEl)=>{
+    if(!formEl) return
+    formEl.validate((valid) =>{
+      if(!valid) return
+      console.log(ruleForm);
+    })
   }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+  .container{
+    @apply min-h-screen bg-purple-400;
+  }
 </style>
