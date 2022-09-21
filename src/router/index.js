bec41b57f2fa2df1c200f6dd308442470d1d9f1f
黄立灼 <1637556100@qq.com>
2022-09-21 18:27:26 +0800

    全局前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import {createRouter,createWebHashHistory} from 'vue-router'
 import IndexRouter from './modules/index.js'
 import LoginRouter from './modules/login.js'
 import NotfoundRouter from '@/views/notfound/index.vue'
+import { getToken } from '@/composables/auth'
+import { toast } from '@/composables/utils'
 
 const routes = [
     IndexRouter,
@@ -24,5 +26,20 @@ const routes = [
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[...routes]
+})
+//路由前置全局前置守卫
+router.beforeEach((to, from, next) =>{
+    const token = getToken()
+    //未登录，强制跳转到登录页面
+    if(!token && to.path != '/login'){
+        toast('请先登录','error')
+        return next({ path: '/login'})
+    }
+    //防止重复登录
+    if(token && to.path == '/login'){
+        toast('请勿重复登录','error')
+        return next({ path: from.path ? from.path : '/'})
+    }
+    next()
 })
 export default router
